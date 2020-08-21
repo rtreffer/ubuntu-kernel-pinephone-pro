6fa18b74c827da330c55f7be942a1889b5438943
Kai-Heng Feng <kai.heng.feng@canonical.com>
2020-08-21 22:34:41 +0800

    UBUNTU: SAUCE: PCI/ASPM: Enable ASPM for links under VMD domain
    BugLink: https://bugs.launchpad.net/bugs/1889384
    
    New Intel laptops with VMD cannot reach deeper power saving state,
    renders very short battery time.
    
    As BIOS may not be able to program the config space for devices under
    VMD domain, ASPM needs to be programmed manually by software. This is
    also the case under Windows.
    
    The VMD controller itself is a root complex integrated endpoint that
    doesn't have ASPM capability, so we can't propagate the ASPM settings to
    devices under it. Hence, simply apply ASPM_STATE_ALL to the links under
    VMD domain, unsupported states will be cleared out anyway.
    
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -5867,8 +5867,20 @@ DECLARE_PCI_FIXUP_ENABLE(PCI_VENDOR_ID_PERICOM, 0x2303,
 DECLARE_PCI_FIXUP_RESUME(PCI_VENDOR_ID_PERICOM, 0x2303,
 			 pci_fixup_pericom_acs_store_forward);
 
+/*
+ * Device [8086:9a09]
+ * BIOS may not be able to access config space of devices under VMD domain, so
+ * it relies on software to enable ASPM for links under VMD.
+ */
+static void pci_fixup_enable_aspm(struct pci_dev *pdev)
+{
+	pdev->dev_flags |= PCI_DEV_FLAGS_ENABLE_ASPM;
+}
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_INTEL, 0x9a09, pci_fixup_enable_aspm);
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_INTEL, 0xa0b0, pci_fixup_enable_aspm);
+
 static void nvidia_ion_ahci_fixup(struct pci_dev *pdev)
 {
-	pdev->dev_flags |= PCI_DEV_FLAGS_HAS_MSI_MASKING;
+        pdev->dev_flags |= PCI_DEV_FLAGS_HAS_MSI_MASKING;
 }
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_NVIDIA, 0x0ab8, nvidia_ion_ahci_fixup);
